7c3238cfd9c536f4339aa748dc85f5212b78b176
NatFrontend <81472167+NatFrontend@users.noreply.github.com>
2021-07-24 20:51:48 +0200

    Added all the project files

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Hi, this project is simplebut for beginner it is quite enjoyable to have a project to work on with some issues (intentionnal or not).
+It can help to discover github or just practice basic front-end development skills.
---
+++ index.js
@@ -0,0 +1,26 @@
+const userposition$ = document.getElementById("boilerplate");
+
+let position1 = [];
+
+//Collect the position
+function myFunction () {
+    try {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } catch {
+        userposition$ = alert("You need to allow localisation");
+    }
+}
+
+//Create the position sentence
+function showPosition (position) {
+    position1.push("Latitude : " + position.coords.latitude + " Longitude : " + position.coords.longitude );
+
+    return resultLocalisation() ;
+}
+
+//Make the position appear on screen
+function resultLocalisation () {
+    if (position1.length > 0) {
+        document.getElementById("result").innerHTML = position1;
+    }
+}
